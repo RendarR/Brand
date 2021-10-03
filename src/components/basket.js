f97fa0b737ac292750af1955d8fac45aa7e55778
Rendar_R <podminogin.alex@yandex.ru>
2021-10-03 20:54:02 +0300

    Исправил конфигурацию webpack. Начинаю раюотать с index.js

---
+++ src/components/basket.js
@@ -0,0 +1,104 @@
+class BasketItem{
+    constructor(item){
+        this.item = item;
+    }
+    render(){
+        return`
+            <div class="drop-cart-products-1">
+                <img src="${this.item.productImg}" width="85" height="100" alt="" class="drop-cart-products-1-img">
+                <div class="drop-cart-products-1-text">
+                    <h3 class="drop-cart-products-1-text-h3">${this.item.productName}</h3>
+                    <img src="../src/assets/imgs/stars.png" alt="" class="drop-cart-products-1-text-stars">
+                    <div class="drop-cart-products-1-text-totallist">${this.item.amount}
+                <span>x</span> $${this.item.productPrice}</div>
+                    </div>
+                    <div class="drop-cart-products-1-delete"><img src="../src/assets/imgs/x.png" alt=""></div>
+                </div>
+            </div>
+            `
+    }
+    sum(){
+        return`
+        ${this.item.amount * this.item.productPrice}
+        `
+    }
+
+}
+ export default class Basket {
+     constructor(container = '#basket', url = '/basket.json'){
+        this.items = [];
+        this.container = document.querySelector(container);
+        this.containerItems = document.querySelector('#basket-items');
+        this.totalPrice = document.querySelector('#price');
+        this.price = document.querySelector('#total');
+        this.shown = false;
+        this.url = 'https://raw.githubusercontent.com/kellolo/static/master/JSON/' + url;
+        this.init();
+     }
+    init() {
+        // this.container = document.querySelector('#basket');
+        // this.containerItems = document.querySelector('#basket-items');
+        this._get(this.url)
+            .then(basket => {
+                this.items = basket.content;
+            })
+            .finally(() => {
+                this._render();
+                this._handleActions();
+                this._renderSum();
+            })
+
+    }
+
+    _get(url) {
+        return fetch(url).then(d => d.json());
+    }
+
+    _render() {
+        let htmlStr = '';
+        this.items.forEach(item => {
+            htmlStr += new BasketItem(item).render();
+        });
+        this.container.innerHTML = htmlStr;
+    }
+    _renderSum(){
+        let sum = 0;
+        this.items.forEach(item => {
+            sum += +new BasketItem(item).sum();
+        });
+        this.price.innerHTML = sum + "$";
+
+    }
+    _handleActions() {
+
+        document.querySelector('#basket-toggler').addEventListener('click', () => {
+            this.container.classList.toggle('invisible');
+            this.totalPrice.classList.toggle('invisible');
+            this.shown = !this.shown;
+        });
+        this.container.addEventListener('click', ev => {
+            if (ev.target.name === 'remove') {
+                this._remove(ev.target.dataset.id);
+            }
+        })
+        
+    }
+    add(item) {
+        let find = this.items.find(el => el.productId === item.productId);
+        if (find) {
+            find.amount++;
+        } else {
+            this.items.push(item);
+        }
+        this._render();
+    }
+    _remove(id) {
+        let find = this.items.find(el => el.productId === id);
+        if (find.amount > 1) {
+            find.amount--;
+        } else {
+            this.items.splice(this.items.indexOf(find), 1);
+        }
+        this._render();
+    }
+ };
---
+++ src/components/catalog.js
@@ -0,0 +1,83 @@
+class CatalogItem{
+    constructor(item){
+        this.item = item;
+    }
+    
+    render() {
+        return `
+        <div class="item">
+            <a href="#" class="item_">
+                <div class="item__">
+                    <div class="img_item">
+                    <div class="open_text">
+                            <div class="gen_add_to_card">
+                                <img src="../src/assets/imgs/1287.png" alt="">
+                                <p class="p_add_to_card">Add to card</p>
+                            </div>
+                        </div>
+                        <img src="${this.item.productImg}" alt="">
+                    </div>
+                    <div class="footer_item">
+                        <h1 class="h1_item">${this.item.productName}</h1>
+                        <h2 class="h2_item">$${this.item.productPrice}</h2>
+                    </div>
+                </div>
+            </a>
+        </div>`
+    }
+}
+
+export default class Catalog {
+    constructor(basket, container = '#catalog', url = '/catalog.json') {
+        this.container = document.querySelector(container);
+        this.items = [];
+        this.basket = basket;
+        this.url = 'https://raw.githubusercontent.com/kellolo/static/master/JSON' + url;
+        this.init();
+    }
+    init() {
+        this._get(this.url)
+            .then(arr => {
+                this.items = arr;
+            })
+            .finally(() => {
+                this._render();
+                this._handleActions();
+            })
+    }
+
+    _get(url) {
+        return fetch(url).then(d => d.json());
+    }
+
+    _fillCatalog() { //Инкапсуляция (условная для JS)
+        this.items = getArrayOfObjects();
+    }
+
+    _render() {
+        let htmlStr = '';
+        this.items.forEach(item => {
+            htmlStr += new CatalogItem(item).render();
+        });
+        this.container.innerHTML = htmlStr;
+    }
+
+    _handleActions() {
+        this.container.addEventListener('click', ev => {
+            if (ev.target.name == 'add') {
+                let dataset = ev.target.dataset;
+                this.basket.add(this._createNewItem(dataset));
+            }
+        })
+    }
+
+    _createNewItem(dataset) {
+        return {
+            productId: dataset.id,
+            productName: dataset.name,
+            productImg: dataset.img,
+            productPrice: +dataset.price,
+            amount: 1
+        }
+    }
+}
